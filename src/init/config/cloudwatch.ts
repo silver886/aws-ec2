4f153445126872d43e7ce6a341d3d16e562f3480
Leo Liu <karta1041337@gmail.com>
2021-07-24 23:17:01 +0800

    Refactor with enum

--- src/init/config/cloudwatch.ts
+++ src/init/config/cloudwatch.ts
@@ -2,65 +2,66 @@ import * as ec2 from '@aws-cdk/aws-ec2';
 
 import * as file from '../file/';
 
-export function agent(arch: string, os: string, obj: unknown): ec2.InitConfig {
-    if (arch !== 'amd64' && arch !== 'arm64') throw new Error('Arch must be `amd64` or `arm64`.');
+export enum Arch {
+    X86_64 = 'amd64',
+    ARM64 = 'arm64',
+}
 
-    let ext = '';
+export enum Os {
+    AMAZON_LINUX_AND_AMAZON_LINUX_2 = 'amazon_linux',
+    CENTOS = 'centos',
+    REDHAT = 'redhat',
+    SUSE = 'suse',
+    ORACLE = 'oracle_linux',
+    WINDOWS = 'windows',
+}
+
+enum Ext {
+    RPM = 'rpm',
+    MSI = 'msi',
+}
+
+export function agent(arch: Arch, os: Os, obj: unknown): ec2.InitConfig {
+    let ext: Ext | null = null;
     switch (arch) {
-        case 'amd64':
-            if (os !== 'amazon_linux' && os !== 'centos' && os !== 'redhat' && os !== 'suse' && os !== 'debian' && os !== 'ubuntu' && os !== 'oracle_linux' && os !== 'windows') throw new Error('OS must be `amazon_linux`, `centos`, `redhat`, `suse`, `debian`, `ubuntu`, `oracle_linux`, or `windows`.');
-
+        case Arch.X86_64:
             switch (os) {
-                case 'amazon_linux':
-                case 'centos':
-                case 'redhat':
-                case 'suse':
-                case 'oracle_linux':
-                    ext = 'rpm';
+                case Os.AMAZON_LINUX_AND_AMAZON_LINUX_2:
+                case Os.CENTOS:
+                case Os.REDHAT:
+                case Os.SUSE:
+                case Os.ORACLE:
+                    ext = Ext.RPM;
                     break;
-                case 'debian':
-                case 'ubuntu':
-                    ext = 'deb';
-                    break;
-                case 'windows':
-                    ext = 'msi';
+                case Os.WINDOWS:
+                    ext = Ext.MSI;
                     break;
 
                     // No default
             }
             break;
-        case 'arm64':
-            if (os !== 'amazon_linux' && os !== 'redhat' && os !== 'ubuntu' && os !== 'suse') throw new Error('OS must be `amazon_linux`, `redhat`, `ubuntu`, or `suse`.');
-
+        case Arch.ARM64:
             switch (os) {
-                case 'amazon_linux':
-                case 'redhat':
-                case 'suse':
-                    ext = 'rpm';
+                case Os.AMAZON_LINUX_AND_AMAZON_LINUX_2:
+                case Os.REDHAT:
+                case Os.SUSE:
+                    ext = Ext.RPM;
                     break;
-                case 'ubuntu':
-                    ext = 'deb';
-                    break;
-
-                // No default
+                default:
+                    throw new Error(`OS is not supported in ${arch}.`);
             }
             break;
 
         // No default
     }
 
-    if (ext !== 'deb' && ext !== 'msi' && ext !== 'rpm') throw new Error('Extension must be `deb`, `msi`, or `rpm`.');
-
     let initPackageGenerator: ((packageName: string, options?: ec2.NamedPackageOptions) => ec2.InitPackage) | null = null;
     switch (ext) {
         /* eslint-disable @typescript-eslint/unbound-method */
-        case 'deb':
-            initPackageGenerator = ec2.InitPackage.apt;
-            break;
-        case 'msi':
+        case Ext.MSI:
             initPackageGenerator = ec2.InitPackage.msi;
             break;
-        case 'rpm':
+        case Ext.RPM:
             initPackageGenerator = ec2.InitPackage.rpm;
             break;
         /* eslint-enable @typescript-eslint/unbound-method */
